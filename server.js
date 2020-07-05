6c139e6d1a29ffe3c56893fd5a081c2801437327
Benjamin Koehler <bkoehler751@gmail.com>
2020-07-05 13:04:08 -0700

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const cors = require('cors');
 const MovieRouter = require('./router/movieRouter.js');
 
 const server = express()
-
+server.use(cors());
 server.use(express.json());
 server.use("/api/movies", MovieRouter);
 
