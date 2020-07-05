446601eee94ab22fc41f33dc99c101651d14286d
Benjamin Koehler <bkoehler751@gmail.com>
2020-07-05 11:20:46 -0700

    added env file and gitignore

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const server = require('./server.js');
+// it's recommended to load configuration for .env as early as possible
+require('dotenv').config(); // add this line as the first thing to run1
 
-const PORT = 9000;
+const server = require('./server');
 
-server.listen(PORT, () => {
-    console.log(`Server running on ${PORT}...`)
-})
+// we'll read the port from the server environment if it is there
+// heroku will have the PORT environment variable set
+const port = process.env.PORT || 5000;
+
+// we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
