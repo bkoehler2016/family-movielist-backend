e07899f607b881565b745639fb8b6371a0a2df84
Benjamin Koehler <bkoehler751@gmail.com>
2020-07-05 18:15:36 -0700

    added post endpoint

--- router/movieRouter.js
+++ router/movieRouter.js
@@ -14,5 +14,26 @@ movieRouter.get("/", (request, response) => {
         console.log("Error: ", error)
         response.status(500).json({message: 'Failed to get movies'})
     })
+
+    movieRouter.post("/",(request, response) => {
+        const movieInfo = request.body;
+        if(!movieInfo.title || !movieInfo.release_year || !movieInfo.rating || !movieInfo.owner || !movieInfo.format){
+            response.status(400).json({
+                errorMessage: 'Please enter all the information'
+            });
+        } else{
+            Movies.insert(movieInfo)
+            .then(post => {
+                response.status(201).json(movie)
+            })
+            .catch(error => {
+                console.log("Error: " , error);
+                response.status(500).json({
+                    error: 'There was an error while saving  the movie to the database.'
+                })
+                
+            })
+        }
+    })
   });
   module.exports = movieRouter
