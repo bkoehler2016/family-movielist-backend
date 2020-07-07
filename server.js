7a5aae7759b2908a6a59c33eb286542bb49e6d6c
Benjamin Koehler <bkoehler751@gmail.com>
2020-07-06 19:01:05 -0700

    added body-parser and cors policy

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const MovieRouter = require('./router/movieRouter.js');
+import bodyParser from 'body-parser';
 
 const server = express()
-server.use(cors());
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
+  
+server.use(bodyParser.urlencoded({extended: false}));
+server .use(bodyParser.json());
 server.use(express.json());
 server.use("/api/movies", MovieRouter);
 
